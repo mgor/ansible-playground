0d8796798636cc6ab13df2ef6b0d3cc7cc816ac5
Mikael Göransson <github@mgor.se>
2020-06-11 21:12:13 +0200

    moved password out of users-role vault, hash them in tasks

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This is my ansible playground. Based on free online courses:
 
 - [Server For Hackers - Ansible](https://serversforhackers.com/s/ansible)
+- [Ansible tutorial for Beginners, Online Ansible Free Training](https://www.learnitguide.net/2018/06/ansible-tutorial-for-beginners-online.html)
 
 ## ansible-vault
 
--- create-environment.bash
+++ create-environment.bash
@@ -9,14 +9,20 @@ main() {
                 ssh-keygen -t ed25519 -f ./.ssh/id_ansible -q N ""
                 multipass mount ./ ansible00:/home/ubuntu/ansible
                 multipass exec ansible00 -- bash <<EOF
-  ssh-agent bash
-  ssh-add /home/ubuntu/ansible/.ssh/id_ansible
-  sudo apt install -y software-properties-common
-  sudo add-apt-repository -y ppa:ansible/ansible
-  sudo apt update
-  sudo apt install -y ansible
+ssh-agent bash
+ssh-add /home/ubuntu/ansible/.ssh/id_ansible
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt update
+sudo apt install -y ansible
 EOF
             fi
+
+            multipass copy-files id_ansible.pub "ansible0${i}":/home/ubuntu/id_ansible.pub
+            multipass exec "ansible0${i}" -- bash <<EOF
+cat ~/id_ansible.pub >> ~/.ssh/authorized_keys
+rm ~/id_ansible.pub
+EOF
         fi
     done
 
